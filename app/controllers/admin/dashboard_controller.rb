69142f2684068c9add4fdd97ba998e05b7e23be0
Rob <robpolisena@gmail.com>
2020-08-19 20:43:18 +0000

    updated Admin Dashboard to show counts of products in the database and count of categories in the database

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['username'], password: ENV['password'], except: :index
+
   def show
+    @products = Product.count(:all)
+    @categories = Category.count(:all)
   end
 end
