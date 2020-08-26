19408afb8eb7fa06916a4a826acb0cb073abb27f
Rob <robpolisena@gmail.com>
2020-08-26 22:00:24 +0000

    created docs folder for screen shots and updated README.md

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ A mini e-commerce application built with Rails 4.2 for purposes of teaching Rail
 
 ## Stripe Testing
 
-Use Credit Card # 4111 1111 1111 1111 for testing success scenarios.
+Use Credit Card # 4111 1111 1111 1111 with any future expiry date and security code for seeing success scenarios.
 
 More information in their docs: <https://stripe.com/docs/testing#cards>
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  
+  private
+  
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
@@ -12,7 +14,6 @@ class ApplicationController < ActionController::Base
     redirect_to '/login' unless current_user
   end
 
-  private
 
   def cart
     @cart ||= cookies[:cart].present? ? JSON.parse(cookies[:cart]) : {}
